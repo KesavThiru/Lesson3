9ca6052a08581d15478d22d51998f94b288cc54c
KesavThiru <kesav.thiru@hotmail.com>
2025-09-30 17:01:32 +1000

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+a=6
+print ("datatype of a:",type(a))
+b=8.9
+print ("datatype of b:",type(b))
+c=True
+print ("datatype of c:",type(c))
+d="Hello"
+print ("datatype of d:",type(d))
+print ("after typecasting")
+a=str(a)
+print ("datatype of a:",type(a))
+b=int(b)
+print ("datatype of b:",type(b))
